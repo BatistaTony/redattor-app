5398688de8bc5f1ad2c5f2545ea3d7033aa53002
Jose Tone <developer.tony17@gmail.com>
2022-10-14 01:01:01 +0100

    setup: project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Redator App</title>
+      <meta name="description" content="Redator app by AndromedaLabs" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main className={styles.main}>
+      <h1 className={styles.title}>Redator App</h1>
+
+      <p className={styles.description}>
+        by <code className={styles.code}>AndromedaLabs</code>
+      </p>
+    </main>
+  </div>
+);
+
+export default Home;
