1a444cfe94707b37a17ef6c124c13d89f15a3f81
Batista Oliveira <batistatomeoliveira96@gmail.com>
2022-11-04 23:05:09 +0100

    feat: change record

--- src/components/molecules/DashboardCardWelcome/index.tsx
+++ src/components/molecules/DashboardCardWelcome/index.tsx
@@ -1,47 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Typography, Button } from '@mui/material';
 import { BlogginPanaIllustration } from 'assets/illustration';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import { getUserInfo } from '@services/user/getUserInfo';
 import { DahsboardCardWelcomeContainer } from './styles';
 
-const DahsboardCardWelcome = () => {
-  const [file, setFile] = useState('');
-
-  return (
-    <DahsboardCardWelcomeContainer>
-      <div className="card-bg" />
-      <div className="content-contaienr">
-        <div className="card-ctr-dash">
-          <Typography
-            variant="h1"
-            sx={{
-              fontSize: '2rem',
-              color: '#fff',
-              lineHeight: '150%',
-              fontWeight: '400',
-            }}
-          >
-            Bem vindo,{' '}
-            <span style={{ fontWeight: '700' }}>Batista Oliveira !</span>
-          </Typography>
-          <Button sx={{ textTransform: 'none' }} className="btn-card-ctr">
-            <SaveAltIcon /> Exportar dados Dash
-          </Button>
-        </div>
+const DahsboardCardWelcome = () => (
+  <DahsboardCardWelcomeContainer>
+    <div className="card-bg" />
+    <div className="content-contaienr">
+      <div className="card-ctr-dash">
         <Typography
-          variant="body1"
-          sx={{ fontSize: '1.26rem', color: '#fff', lineHeight: '150%' }}
+          variant="h1"
+          sx={{
+            fontSize: '2rem',
+            color: '#fff',
+            lineHeight: '150%',
+            fontWeight: '400',
+          }}
         >
-          O E-redattor é o seu espaço único para gerenciar as escritas dos seus
-          projetos jornalisticos, com diversos colunistas e validadores
-          trabalhando em tempo real.
+          Bem vindo
+          <span style={{ fontWeight: '700' }}>!</span>
         </Typography>
+        <Button sx={{ textTransform: 'none' }} className="btn-card-ctr">
+          <SaveAltIcon /> Exportar dados Dash
+        </Button>
       </div>
-      <div className="illustration-blog">
-        <BlogginPanaIllustration />
-      </div>
-    </DahsboardCardWelcomeContainer>
-  );
-};
+      <Typography
+        variant="body1"
+        sx={{ fontSize: '1.26rem', color: '#fff', lineHeight: '150%' }}
+      >
+        O E-redattor é o seu espaço único para gerenciar as escritas dos seus
+        projetos jornalisticos, com diversos colunistas e validadores
+        trabalhando em tempo real.
+      </Typography>
+    </div>
+    <div className="illustration-blog">
+      <BlogginPanaIllustration />
+    </div>
+  </DahsboardCardWelcomeContainer>
+);
 
 export default DahsboardCardWelcome;
